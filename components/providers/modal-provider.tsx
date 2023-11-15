a40f6b9a216aad700831dddf5cd5f9b6febe09f1
nahuelvera00 <nahuelvera3770@gmail.com>
2023-11-15 18:31:44 -0300

    added create-channel functions

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,6 +7,7 @@ import { CreateServerModal } from "../modals/create-server-modal";
 import { InviteModal } from "../modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-modal";
+import { CreateChannelModal } from "../modals/create-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -22,6 +23,7 @@ export const ModalProvider = () => {
   return (
     <>
       <CreateServerModal />
+      <CreateChannelModal />
       <InviteModal />
       <EditServerModal />
       <MembersModal />
