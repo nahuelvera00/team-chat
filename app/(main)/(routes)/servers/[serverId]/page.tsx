f252f8015c24ed75c3a4065ca339eb3c17462214
Nahuel Vera <42484308+nahuelvera00@users.noreply.github.com>
2024-01-17 00:33:57 -0300

    Revert "channel page redirect"

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,49 +1,5 @@
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
-import { redirectToSignIn } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
-
-interface ServerIdProps {
-  params: {
-    serverId: string;
-  };
-}
-
-const ServerIdPage = async ({ params }: ServerIdProps) => {
-  const profile = await currentProfile();
-
-  if (!profile) {
-    return redirectToSignIn();
-  }
-
-  const server = await db.server.findUnique({
-    where: {
-      id: params.serverId,
-      members: {
-        some: {
-          profileId: profile.id,
-        },
-      },
-    },
-    include: {
-      channels: {
-        where: {
-          name: "general",
-        },
-        orderBy: {
-          createdAt: "asc",
-        },
-      },
-    },
-  });
-
-  const initialChannel = server?.channels[0];
-
-  if (initialChannel?.name !== "general") {
-    return null;
-  }
-
-  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`);
+const ServerPage = () => {
+  return <div>server page</div>;
 };
 
-export default ServerIdPage;
+export default ServerPage;
